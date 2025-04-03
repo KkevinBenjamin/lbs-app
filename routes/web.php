03a379609f8db1ea6a802015d44261d823f75c17
kevin <blackrock.dev009@gmail.com>
2025-04-03 23:45:03 +0900

    MVC

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get(uri: '/', action: function (): View {
+    return view(view: 'welcome');
 });
+Route::apiResource(name: '/student', controller: StudentController::class);
